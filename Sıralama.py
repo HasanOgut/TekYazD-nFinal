2e9236a4b263caf003bd75b41104ccc78ec1bdf6
Hasan Ogut <oguthsn199@gmail.com>
2024-12-25 23:11:18 +0300

    Küçükten büyüğe sıralama algoritması yazıldı

---
+++ Sıralama.py
@@ -0,0 +1,13 @@
+import random
+
+sayilar = [random.randint(1, 100) for _ in range(20)]
+
+print("Orijinal Dizi:", sayilar)
+
+for i in range(len(sayilar)):
+    for j in range(i + 1, len(sayilar)):
+        if sayilar[i] > sayilar[j]:
+            sayilar[i], sayilar[j] = sayilar[j], sayilar[i]
+
+
+print("Küçükten Büyüğe Sıralı Dizi:", sayilar)
